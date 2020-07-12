c6e43a9682deeb8f33feebeccd1b39008b1d2760
Sarel Weinberger <sarelw1@gmail.com>
2020-07-12 09:06:34 +0300

    more abuot filtering and compering data frames

--- filtering.py
+++ filtering.py
@@ -1,7 +1,17 @@
-import  stak_overflow_data as sod
-import  pandas as pd
+import stak_overflow_data as sod
+import pandas as pd
 
 filt = sod.data_frame['Hobbyist'] == 'Yes'
 filt_df = pd.DataFrame(filt)
-#sod.data_frame(filt)
-print(sod.data_frame.head())
+#or
+df1 = sod.data_frame[filt]
+print(type(df1))
+#or
+df2 = sod.data_frame[sod.data_frame['Hobbyist'] == 'Yes']
+print(type(df2))
+# compere two df
+# if df1 & df2:
+#     print('the same way of filtering')
+#print(pd.data_frame(filt))
+
+#print(sod.data_frame.head())
--- stak_overflow_data.py
+++ stak_overflow_data.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
+#StackOverflow Survey Download Page - http://bit.ly/SO-Survey-Download
 data_frame = pd.read_csv(r'C:\Users\User\Desktop\git_clone\survey_results_public.csv', index_col='UndergradMajor')
 schema_df = pd.read_csv((r'C:\Users\User\Desktop\git_clone\survey_results_schema.csv'))
 #print(data_frame.info())
 
 pd.set_option('display.max_columns', 85)
 pd.set_option('display.max_rows', 85)
-print(schema_df)
+#print(schema_df)
 #print(data_frame.head(2))
 
 data_frame['Hobbyist'].value_counts()
